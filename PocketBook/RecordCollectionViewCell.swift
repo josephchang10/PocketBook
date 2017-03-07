116f84e4b61b440b0b418d9878092ffcfa6c286d
张嘉夫 <joseph@fanruan.com>
2017-03-07 19:52:01 +0800

    UI改进

--- PocketBook/RecordCollectionViewCell.swift
+++ PocketBook/RecordCollectionViewCell.swift
@@ -22,6 +22,7 @@ class RecordCollectionViewCell: UICollectionViewCell {
         self.contentView.layer.borderColor = UIColor.clear.cgColor
         self.contentView.layer.masksToBounds = true
         
+        self.layer.cornerRadius = 10
         self.layer.shadowColor = UIColor.lightGray.cgColor
         self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
         self.layer.shadowRadius = 10.0
