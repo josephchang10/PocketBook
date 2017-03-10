09605a5094b93602c9dad36db38c735758a632b0
张嘉夫 <joseph@fanruan.com>
2017-03-10 11:56:49 +0800

    Add record button redesign

--- PocketBook/AddRecordButton.swift
+++ PocketBook/AddRecordButton.swift
@@ -14,18 +14,12 @@ class AddRecordButton: UIButton {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
+        super.draw(rect)
         // Drawing code
-        self.layer.cornerRadius = 10
-        self.layer.borderWidth = 1.0
-        self.layer.borderColor = UIColor.clear.cgColor
-        self.layer.masksToBounds = true
-        
-        self.layer.shadowColor = UIColor.lightGray.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
-        self.layer.shadowRadius = 5.0
-        self.layer.shadowOpacity = 0.3
-        self.layer.masksToBounds = false
-        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.layer.cornerRadius).cgPath
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOffset = CGSize(width: 3, height: 3)
+        layer.shadowRadius = 10
+        layer.shadowOpacity = 0.3
     }
     
 
